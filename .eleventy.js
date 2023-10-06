ac80e01c018801696bdf96a238d0897ca9cf475c
Dan Carter <djcarter85@gmail.com>
2023-10-06 18:43:31 +0100

    Include CNAME in build for custom domain

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addCollection("posts", function (collection) {
     return collection.getFilteredByGlob("_posts/*.md");
